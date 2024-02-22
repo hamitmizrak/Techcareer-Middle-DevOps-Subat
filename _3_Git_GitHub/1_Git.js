80750e8393331beff9cbc61a6fb1327b6f94f256
Hamit Mizrak <hamitmizrak@gmail.com>
2024-02-22 13:00:33 +0300

    git devam

--- _3_Git_GitHub/1_Git.js
+++ _3_Git_GitHub/1_Git.js
@@ -10,18 +10,23 @@ VCS: Version Control FileSystem
 En önemli özelliği dağıtık mimariye ship olmasıdır.
 
 -------------------------------------
+## INIT ##
 git init
 git status
 git add .
 git commit -m "Dev-Middle-Project"
 git remote add origin URL 
-git push -u origin master
+git push -u origin master (u => --set-upstream)
+
+git push
 
 -------------------------------------
+## CLONE ##
 git clone https://github.com/hamitmizrak/Techcareer-Middle-DevOps-Subat
 
 
 -------------------------------------
+## CONFIG ##
 git config --global user.name
 git config --global user.name "Hamit Mızrak"
 git config --global user.email
@@ -30,10 +35,48 @@ git config --global -l
 git config --global core.editor "notepad++"
 
 -------------------------------------
+## ADD ##
 git --help
-
 git add . 
 git commit -m "Data"
 
+# Add komutunu yazmadan direk commit üzerinden add yapalım.
 git commit -a -m "Data2"
 
+-------------------------------------
+## ALIAS ##
+git log 
+git log --decorate --oneline --graph --all
+git config --global alias.log44 "log --decorate --oneline --graph --all"
+git log44
+
+
+-------------------------------------
+## REMOTE ##
+git remote 
+URL Değiştirmek => git remote set-url origin2 yeniURL
+
+
+-------------------------------------
+## CHECKOUT/SWITCH ##
+
+-------------------------------------
+## STASH ##
+
+-------------------------------------
+## BRANCH ##
+
+-------------------------------------
+## MERGE/REBASE ##
+
+-------------------------------------
+## PULL/FETCH ##
+
+-------------------------------------
+## TAG ##
+
+-------------------------------------
+## CONFLICT ##
+
+-------------------------------------
+## SSH-KEYGEN ##
