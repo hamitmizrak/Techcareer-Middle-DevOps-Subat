214f7d8b9f7adcf7b9c15f1bb7cb3b680c9ad0b8
Hamit Mizrak <hamitmizrak@gmail.com>
2024-02-22 12:47:45 +0300

    git

--- _3_Git_GitHub/1_Git.js
+++ _3_Git_GitHub/1_Git.js
@@ -1,4 +1,14 @@
-Git: Vcs 
+Git: 
+2005 Linus Torvalds
+Ücretsiz 
+Açık kaynak kodludur.
+YEdekleme imkanı sağlar 
+Proje takibini sağlar.
+Birden fazla kişilerle çalışmayı sağlar.
+Global Information Tracked
+VCS: Version Control FileSystem
+En önemli özelliği dağıtık mimariye ship olmasıdır.
+
 -------------------------------------
 git init
 git status
@@ -9,3 +19,16 @@ git push -u origin master
 
 -------------------------------------
 git clone https://github.com/hamitmizrak/Techcareer-Middle-DevOps-Subat
+
+
+-------------------------------------
+git config --global user.name
+git config --global user.name "Hamit Mızrak"
+git config --global user.email
+git config --global user.email "hamitmizrak@gmail.com"
+git config --global -l 
+git config --global core.editor "notepad++"
+
+-------------------------------------
+git --help
+
