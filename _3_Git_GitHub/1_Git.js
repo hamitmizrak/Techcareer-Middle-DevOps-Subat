52d4b20bf2a2adc7b54d1c50e3c2b3614f7b01ca
Hamit Mizrak <hamitmizrak@gmail.com>
2024-02-22 12:38:03 +0300

    revize

--- _3_Git_GitHub/1_Git.js
+++ _3_Git_GitHub/1_Git.js
@@ -1,5 +1,5 @@
 Git: Vcs 
-------------
+-------------------------------------
 git init
 git status
 git add .
@@ -7,5 +7,5 @@ git commit -m "Dev-Middle-Project"
 git remote add origin URL 
 git push -u origin master
 
------------ 
+-------------------------------------
 git clone https://github.com/hamitmizrak/Techcareer-Middle-DevOps-Subat
