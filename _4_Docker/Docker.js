52ab41f9f22b533c5876fdecd590c3c2355071e0
Hamit Mizrak <hamitmizrak@gmail.com>
2024-02-26 10:44:54 +0300

    Docker

---
+++ _4_Docker/Docker.js
@@ -0,0 +1,30 @@
+Bare-Metal (Windows 10) 
+Virtual Machine ()
+Container
+
+-------------------------- 
+Docker Nedir ?
+LXC : LinuX Containers LXC () Linux Container üzerine kurulu bir container yapısıdır.
+Docker 2008 Yılında Soloman Hykes 
+2013 Docker Kuruldu 
+2014 Docker 1.0
+2015 Docker Swarm (Orchestration)
+Go diliyle 
+Open source
+DotCloud =>  Docker INC
+
+Docker CE Major 6, Destek sağlanıyor
+Docker EE Major 12-18 ay, Destek sağlanıyor
+
+Docker 
+---------------
+Hızlı Derleme 
+Dağıtım 
+Paket Sistemei
+Ölçeklendirme (Orch...)
+Test Etme
+
+Docker Hub : Image Al/Ver
+
+Docker Platform 
+Windows, Linux, MAcOS
